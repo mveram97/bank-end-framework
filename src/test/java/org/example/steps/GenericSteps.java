873ed4666be9723978ee55d75160293b58d9458b
marcotenoriontt <mtenoric@emeal.nttdata.com>
2024-10-01 13:47:14 +0200

    Created function with Given. Created Scenario to Update a user.

--- src/test/java/org/example/steps/GenericSteps.java
+++ src/test/java/org/example/steps/GenericSteps.java
@@ -1,5 +1,6 @@
 package org.example.steps;  // Asegúrate de que el paquete sea correcto
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
@@ -9,6 +10,12 @@ import static org.junit.Assert.assertEquals;
 public class GenericSteps {
   private Response response;
 
+  @Given("A user with ID {int} exists")
+  public void aUserWithIDExists(int userId) {
+    response = RestAssured.get("https://reqres.in/api/users/" + userId);
+    assertEquals(200, response.getStatusCode());  // Asegurarse de que el usuario existe
+  }
+
   @When("I send a {string} to {string}")
   public void iSendATo(String method, String url) {
     if (method.equalsIgnoreCase("POST")) {
@@ -16,6 +23,13 @@ public class GenericSteps {
               .header("Content-Type", "application/json")
               .body("{ \"name\": \"morpheus\", \"job\": \"leader\" }")
               .post(url);
+
+    }
+    else if (method.equalsIgnoreCase("PUT")) {
+      response = RestAssured.given()
+              .header("Content-Type", "application/json")
+              .body("{ \"name\": \"morpheus\", \"job\": \"zion resident\" }")  // Datos de actualización
+              .put(url);
     }
   }
 
--- src/test/resources/features/feature1.feature
+++ src/test/resources/features/feature1.feature
@@ -1,4 +1,8 @@
 Feature: Test API
-  Scenario: Create an user
+  Scenario: Create a user
     When I send a "POST" to "https://reqres.in/api/users"
     Then Result should be 201
+
+  Scenario: Update a user
+    When I send a "PUT" to "https://reqres.in/api/users/2"
+    Then Result should be 200
