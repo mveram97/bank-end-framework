6a9c5bc72792ae1d81a967c3011196b08d8093d0
marcotenoriontt <mtenoric@emeal.nttdata.com>
2024-10-09 10:21:05 +0200

    Start design Database.

---
+++ src/main/java/org/example/entity/Customer.java
@@ -0,0 +1,22 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Account> accounts;
+}
--- src/test/java/org/example/steps/GenericSteps.java
+++ src/test/java/org/example/steps/GenericSteps.java
@@ -1,77 +1,8 @@
-package org.example.steps;  // Asegúrate de que el paquete sea correcto
+package org.example.steps;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
+
 import static org.junit.Assert.assertEquals;
 
 public class GenericSteps {
-  private Response response;
 
-  @Given("A user with ID {int} exists")
-  public void aUserWithIDExists(int userId) {
-    response = RestAssured.get("https://reqres.in/api/users/" + userId);
-    assertEquals(200, response.getStatusCode());  // Asegurarse de que el usuario existe
-  }
-
-  @When("I send a {string} to {string}")
-  public void iSendATo(String method, String url) {
-    if (method.equalsIgnoreCase("POST")) {
-      response = RestAssured.given()
-              .header("Content-Type", "application/json")
-              .body("{ \"name\": \"morpheus\", \"job\": \"leader\" }")
-              .post(url);
-
-    }
-    else if (method.equalsIgnoreCase("PUT")) {
-      response = RestAssured.given()
-              .header("Content-Type", "application/json")
-              .body("{ \"name\": \"morpheus\", \"job\": \"zion resident\" }")  // Datos de actualización
-              .put(url);
-    } else if (method.equalsIgnoreCase("GET")) {
-      response = RestAssured.given()
-              .header("Content-Type", "application/json")
-              .get(url);
-
-    } else if (method.equalsIgnoreCase("DELETE")) {
-      response = RestAssured.given()
-              .header("Content-Type", "application/json")
-              .delete(url);
-    }
-    else if (method.equalsIgnoreCase("GET")){
-      response = RestAssured.get(url);
-    }
-    else if (method.equalsIgnoreCase("DELETE")){
-      response = RestAssured.delete(url);
-    }
-  }
-
-  @Then("Result should be {int}")
-  public void resultShouldBe(int expectedStatusCode) {
-    assertEquals(expectedStatusCode, response.getStatusCode());
-  }
-
-  @And("I have to wait {int} seconds")
-  public void iHaveToWaitSeconds(int seconds) {
-    try {
-      Thread.sleep(seconds * 1000); // Espera el número de segundos especificado
-    } catch (InterruptedException e) {
-      // Manejo de la excepción si el hilo es interrumpido
-      e.printStackTrace();
-    }
-  }
-
-  @When("I send a {string} to {string} with  {string} and {string}")
-  public void iSendAToWithAnd(String method, String url, String email, String password) {
-    if (method.equalsIgnoreCase("POST")) {
-      response = RestAssured.given()
-              .header("Content-Type", "application/json")
-              .body("{ \"email\": \""+ email + "\", \"password\": \""+ password +"\" }")
-              .post(url);
-
-    }
-  }
 }
