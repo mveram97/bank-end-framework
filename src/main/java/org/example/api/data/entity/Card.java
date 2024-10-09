4227a43ff9b25faa6b901a374837a81bae01ecd3
pverderu <pverderu@emeal.nttdata.com>
2024-10-09 11:22:11 +0200

    add card entity

---
+++ src/main/java/org/example/api/data/entity/Card.java
@@ -0,0 +1,25 @@
+package org.example.api.data.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardId;
+
+    private String type;
+    private Long number;
+    private int cvc;
+    private Date expirationDate;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Card> cards;
+
+
+}
