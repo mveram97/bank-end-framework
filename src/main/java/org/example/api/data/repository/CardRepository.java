211a7facf4711017d6fa63b8b620fc901edcc80e
rargando <largando@emeal.nttdata.com>
2024-10-10 11:40:05 +0200

    Endpoint Get all cards by accountId

--- src/main/java/org/example/api/data/repository/CardRepository.java
+++ src/main/java/org/example/api/data/repository/CardRepository.java
@@ -1,13 +1,15 @@
 package org.example.api.data.repository;
 
 import org.example.api.data.entity.Card;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Integer> {
-  Optional<Card> findByCardId(Integer cardId);
+    Optional<Card> findByCardId(Integer cardId);
+
+    List<Card> findByAccount_AccountId(Integer accountId);
 }
