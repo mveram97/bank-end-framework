9c26527181df37c511e85f166ec1532826865b9d
rargando <largando@emeal.nttdata.com>
2024-10-10 11:40:05 +0200

    Endpoint Get all cards by accountId

--- src/main/java/org/example/api/data/controllers/CardController.java
+++ src/main/java/org/example/api/data/controllers/CardController.java
@@ -2,7 +2,6 @@ package org.example.api.data.controllers;
 
 import org.example.api.data.entity.Card;
 import org.example.api.service.CardService;
-import org.example.api.service.CustomerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +11,24 @@ import java.util.Optional;
 
 @RestController
 public class CardController {
-  private CardService card;
+    private CardService card;
 
-  public CardController(CardService card) {
-    this.card = card;
-  }
+    public CardController(CardService card) {
+        this.card = card;
+    }
 
-  @GetMapping("/card")
-  public List<Card> card() {
-    return card.findAll();
-  }
+    @GetMapping("/card")
+    public List<Card> card() {
+        return card.findAll();
+    }
 
-  @GetMapping("/card/{id}")
-  public Optional<Card> card(@PathVariable Integer id) {
-    return card.findById(id);
-  }
+    @GetMapping("/card/{id}")
+    public Optional<Card> card(@PathVariable Integer id) {
+        return card.findById(id);
+    }
+
+    @GetMapping("/cards/{accountId}")
+    public List<Card> cardsByAccountId(@PathVariable Integer accountId) {
+        return card.findByAccountId(accountId);
+    }
 }
