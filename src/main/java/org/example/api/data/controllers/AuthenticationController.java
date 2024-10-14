7314025fba41e625c4c530b932b8f63a143b3af6
alexandrurizeantt <arobertr@emeal.nttdata.com>
2024-10-14 11:25:35 +0200

    LogOut

--- src/main/java/org/example/api/data/controllers/AuthenticationController.java
+++ src/main/java/org/example/api/data/controllers/AuthenticationController.java
@@ -1,25 +1,36 @@
 package org.example.api.data.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.example.api.data.entity.Customer;
 import org.example.api.data.request.LoginRequest;
 import org.example.api.service.AuthService;
 import org.example.api.service.CustomerService;
+import org.example.api.token.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class AuthenticationController {
     @Autowired
     private AuthService authService;
+    @Autowired
+    private Token token;
 
     private final CustomerService customerService;
+
+    private final String jwtCookie = "cookieToken";
 
     public AuthenticationController(CustomerService customerService) {
         this.customerService = customerService;
@@ -48,11 +59,21 @@ public class AuthenticationController {
         return ResponseEntity.badRequest().body("Invalid credentials");
     }
 
-    /*@PostMapping("/logOut")
-    public String logOut(@RequestBody Customer nuevoCust){
 
-        return "The customer has loged out succesfully";
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
 
-    }*/
+        String jwt = authService.getJwtFromCookies(request);
 
+
+        if (jwt == null || !token.validateToken(jwt)){
+             return ResponseEntity.badRequest().body("You have to Log In first");
+
+        }
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie, "")
+                .path("/public").maxAge(0).httpOnly(true).build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body("Logged out successfully. Cookies cleared.");
+    }
 }
