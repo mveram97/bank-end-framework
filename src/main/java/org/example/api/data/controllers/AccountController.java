0eb31f493ce7a85145346fbfdf5804e82109540e
Rafael <ralcantl@emeal.nttdata.com>
2024-10-14 16:14:49 +0200

    Implemented new restrictions and improved code

--- src/main/java/org/example/api/data/controllers/AccountController.java
+++ src/main/java/org/example/api/data/controllers/AccountController.java
@@ -38,16 +38,17 @@ public class AccountController {
     private Token tokenService;
 
     @GetMapping("/account/{id}")
-    public Optional<Account> accountById(@PathVariable Integer id){
+    public Optional<Account> accountById(@PathVariable Integer id) {
 
 
         return account.findById(id);
     }
 
     @GetMapping("/accounts/{customerId}")
-    public List<Account> accountsByCustomer(@PathVariable Integer customerId){
+    public List<Account> accountsByCustomer(@PathVariable Integer customerId) {
         return account.findByCustomer(customerId);
     }
+
     @GetMapping("/api/accounts")
     public ResponseEntity<List<Account>> getUserAccounts(HttpServletRequest request) {
         // Obtener el token JWT desde las cookies
@@ -76,47 +77,52 @@ public class AccountController {
     }
 
     @PatchMapping("/transfer")
-    public ResponseEntity<String> localTransfer(@RequestBody Transfer transfer, HttpServletRequest request){
+    public ResponseEntity<String> localTransfer(@RequestBody Transfer transfer, HttpServletRequest request) {
 
-        //Obtiene el customerId y lo pasa al JSON
+        //Obtiene el cliente que esta realizando la peticion
         String jwt = authService.getJwtFromCookies(request);
         String email = Token.getCustomerEmailFromJWT(jwt);
         Customer customer = customerRepository.findByEmail(email).get();
 
-        //Recibe la info del receiver
+        //Recibe la informacion de la cuenta emisora
+        Integer senderAccountId = transfer.getCustomerAccountId();
+        Optional<Account> senderAccountOpt = accountRepository.findByAccountId(senderAccountId);
+        if (!senderAccountOpt.isPresent()) {
+            return ResponseEntity.badRequest().body("No existe la cuenta emisora ");
+        }
+        Account senderAccount = senderAccountOpt.get();
+
+        //Recibe la informacion de la cuenta receiver
         Integer receiverId = transfer.getReceiverId();
-        Optional<Account> receiverOpt = accountRepository.findById(receiverId);
-        if (!receiverOpt.isPresent()){
+        Optional<Account> receiverOpt = accountRepository.findByAccountId(receiverId);
+        if (!receiverOpt.isPresent()) {
             return ResponseEntity.badRequest().body("No existe la cuenta receptora ");
         }
         Account receiver = receiverOpt.get();
 
-
-        //Comprueba si la cuenta pertenece al usuario que hace la transferencia
-        for(Account cuenta : customer.getAccounts()){
-
-            //Si la cuenta esta asociada al usuario que esta haciendo la peticion, entonces sigue con el codigo
-            if(cuenta.getAccountId().equals(transfer.getCustomerAccountId())){
-                Account cuentaEmisora = cuenta;
-
-                //Comprueba si las cuenta receptora esta bloqueada, si la emisora tiene deudas o si se quiere
-                // pasar mas dinero del que hay en la cuenta
-                if (receiver.getIsBlocked() || cuentaEmisora.getIsInDebt()
-                        || cuentaEmisora.getAmount() < transfer.getAmount()){
-                    return ResponseEntity.badRequest().body("No se puede realizar la transferencia");
-                }
-
-                //Realiza la operacion de transferencia del amount
-                cuentaEmisora.setAmount(cuentaEmisora.getAmount() - transfer.getAmount());
-                receiver.setAmount(receiver.getAmount() + transfer.getAmount());
-
-                accountRepository.save(cuentaEmisora);
-                accountRepository.save(receiver);
-                return ResponseEntity.ok().body("La transferencia fue realizada con exito ");
+        //Comprueba la cuenta emisora esta asociada al usuario que esta haciendo la peticion
+        if (senderAccount.getCustomer().equals(customer)) {
+        Double transferAmount = transfer.getAmount();
+            //Comprueba si las cuenta receptora esta bloqueada, si la emisora tiene deudas o si se quiere
+            // pasar mas dinero del que hay en la cuenta
+            if (receiver.getIsBlocked() || senderAccount.getIsInDebt()
+                    || senderAccount.getAmount() < transferAmount) {
+                return ResponseEntity.badRequest()
+                        .body("No se puede realizar la transferencia. Saldo insuficiente o receptor bloqueado.");
+            } else if(transferAmount <= 0){
+                return ResponseEntity.badRequest()
+                        .body("El traspaso debe ser mayor que cero");
             }
-        }
 
+            //Realiza la operacion de transferencia del amount
+            senderAccount.setAmount(senderAccount.getAmount() - transferAmount);
+            receiver.setAmount(receiver.getAmount() + transferAmount);
+            accountRepository.save(senderAccount);
+            accountRepository.save(receiver);
+
+            //Devuelve que se ha llevado a cabo la operacion sin problema
+            return ResponseEntity.ok().body("La transferencia fue realizada con exito ");
+        }
         return ResponseEntity.badRequest().body("La cuenta no esta asociada a este usuario");
     }
-
 }
