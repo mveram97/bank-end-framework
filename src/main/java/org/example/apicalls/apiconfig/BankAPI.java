325b50a6b1110f85e7b4a65f12a8c848a7067e54
nataliacastellanontt <natalia.castellanoramos.st@emeal.nttdata.com>
2024-10-15 13:55:14 +0200

    Added apicalls package

---
+++ src/main/java/org/example/apicalls/apiconfig/BankAPI.java
@@ -0,0 +1,24 @@
+package org.example.apicalls.apiconfig;
+
+
+public interface BankAPI {
+
+
+    // AÑADIR TODOS LOS ENDPOINTS DE NUESTRA API
+
+    /*
+    @GET
+    @Path("/v1/storage/files")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response getV1StorageFiles(
+            @HeaderParam("Authorization") String authorization, @QueryParam("q") String appId, @QueryParam("kind") String kind, @QueryParam("per_page") Integer perPage);
+
+    @GET
+    @Path("/rest/v1/info/platforms/{automation_api}")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response getV1InfoPlatformsAutomationApi(
+            @HeaderParam("Authorization") String authorization,
+            @PathParam("automation_api") String automation_api);
+     */
+}
+
---
+++ src/main/java/org/example/apicalls/service/BankService.java
@@ -0,0 +1,3 @@
+package org.example.apicalls.service;
+
+public class BankService {}
