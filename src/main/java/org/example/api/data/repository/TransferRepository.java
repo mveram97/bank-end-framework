73e40d786a213a5a440cad12c9a2335e3b577d24
nataliacastellanontt <natalia.castellanoramos.st@emeal.nttdata.com>
2024-10-15 11:45:37 +0200

    Added TransferRepository

---
+++ src/main/java/org/example/api/data/repository/TransferRepository.java
@@ -0,0 +1,19 @@
+package org.example.api.data.repository;
+
+import org.example.api.data.entity.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Integer> {
+
+    Optional<Transfer> findByTransferId(Integer transferId); // find by transferId
+
+    List<Transfer> findByOriginAccount_AccountId(Integer originAccountId); // find transfers by originAccountId
+
+    List<Transfer> findByReceivingAccount_AccountId(Integer receivingAccountId); // find transfers by receivingAccountId
+
+    List<Transfer> findByTransferStatus(Transfer.TransferStatus transferStatus); // find transfers by status
+}
