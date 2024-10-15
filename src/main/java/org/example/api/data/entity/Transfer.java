fc35dc2a6a2fcc99ac9621da6a88d8a42b170884
nataliacastellanontt <natalia.castellanoramos.st@emeal.nttdata.com>
2024-10-15 11:26:31 +0200

    Added Transfer Entity & Modified Account Entity

--- src/main/java/org/example/api/data/entity/Transfer.java
+++ src/main/java/org/example/api/data/entity/Transfer.java
@@ -1,18 +1,60 @@
 package org.example.api.data.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Entity
 public class Transfer {
-    private Integer customerId;
-    private Integer customerAccountId;
-    private Integer receiverId;
-    private Double amount;
-    private Date operationDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer transferId;
+
+    @Column(nullable = false)
+    private Double transferAmount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CurrencyType currencyType;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransferStatus transferStatus;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)   // fecha y hora
+    private Date transferDate;
+
+    @JsonIgnore
+    @ManyToOne      // originAccount -> FK a Account
+    @JoinColumn(name = "origin_account_id", nullable = false)
+    private Account originAccount;  // 1 originAccount - N transfers
+
+    @JsonIgnore
+    @ManyToOne      // receivingAccount -> FK a Account
+    @JoinColumn(name = "receiving_account_id", nullable = false)
+    private Account receivingAccount;  // 1 receivingAccount - N transfers
+
+    // Definición del enumerado CurrencyType
+    public enum CurrencyType {
+        USD,
+        EUR
+    }
+
+    // Definición del enumerado TransferStatus
+    public enum TransferStatus {
+        PENDING,
+        FAILED,
+        SUCCESSFUL
+    }
+
+    // Constructor
+    public Transfer() {
+        this.transferDate = new Date(); // Inicializa con la fecha actual del sistema
+        this.transferStatus = TransferStatus.PENDING; // Estado inicial pendiente por defecto
+    }
 }
