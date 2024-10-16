d9a112198afb8fa7d04057013eb90098a51af2d1
alexandrurizeantt <arobertr@emeal.nttdata.com>
2024-10-16 11:10:51 +0200

    GET all sent transfers from 1 account

--- src/main/java/org/example/api/service/TransferService.java
+++ src/main/java/org/example/api/service/TransferService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -65,6 +66,10 @@ public class TransferService {
 
         // Check operation has been done successfully
         return ResponseEntity.ok().body("Transfer made successfully");
+    }
+
+    public List<Transfer> getTransferByAccountId(Integer originAccountId){
+        return transferRepository.findByOriginAccount_AccountId(originAccountId);
     }
 
 
