ce3557c8e5389951a9693cd08bc1896e815d5ea9
rargando <largando@emeal.nttdata.com>
2024-10-16 11:42:56 +0200

    Endpoints received transfers

--- src/main/java/org/example/api/data/controllers/TransferController.java
+++ src/main/java/org/example/api/data/controllers/TransferController.java
@@ -13,10 +13,13 @@ import org.example.api.service.TransferService;
 import org.example.api.token.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -87,5 +90,80 @@ public class TransferController {
         }
         System.out.println(senderAccount.getCustomer().equals(customer));
         return ResponseEntity.badRequest().body("Account does not belong to the user");
+    }
+
+    @GetMapping("/api/transfers/receive/all")
+    public ResponseEntity<String> getAllReceivedTransfers() {
+        // Verify user
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Assuming username is contained in the customerId
+        Integer customerId = Integer.valueOf(authentication.getName());
+        Optional<Customer> customer = customerRepository.findById(customerId);
+        if (customer.isEmpty())
+            return ResponseEntity.badRequest().body("You are not logged");
+
+        // Verify user has accounts
+        List<Account> accountList = accountRepository.findByCustomer_CustomerId(customerId);
+        if (accountList.isEmpty())
+            return ResponseEntity.badRequest().body("This user does not have any account");
+
+        // Get all the transfers received from all accounts
+        List<Transfer> allTransfersReceived = new ArrayList<>();
+        for (Account account : accountList) {
+            allTransfersReceived.addAll(transferService.findByReceivingAccount(account.getAccountId()));
+        }
+
+        // Inform if there are no transfer received
+        if (allTransfersReceived.isEmpty())
+            return ResponseEntity.ok("This user has not received any transfer");
+
+        // Show every transfer information
+        StringBuilder responseBody = new StringBuilder();
+        for (Transfer transfer : allTransfersReceived) {
+            responseBody.append("{\n\tid: ")
+                    .append(transfer.getTransferId())
+                    .append("\n\tdate: ")
+                    .append(transfer.getTransferDate())
+                    .append("\n\tcurrencyType: ")
+                    .append(transfer.getCurrencyType())
+                    .append("\n\tamount: ")
+                    .append(transfer.getTransferAmount())
+                    .append("\n\tstatus: ")
+                    .append(transfer.getTransferStatus())
+                    .append("\n}\n");
+        }
+        return ResponseEntity.ok(responseBody.toString());
+    }
+
+    @GetMapping("/api/transfer/receive/{accountId}")
+    public ResponseEntity<String> getReceiveTransfer(@PathVariable Integer accountId) {
+        // Verify account exists
+        Optional<Account> account = accountRepository.findByAccountId(accountId);
+        if (account.isEmpty())
+            return ResponseEntity.badRequest().body("Account does not exist");
+
+        // Find all transfer -> inform if we did not find any transfer
+        List<Transfer> receivedTransfers = transferService.findByReceivingAccount(accountId);
+        if (receivedTransfers.isEmpty()) {
+            return ResponseEntity.ok("This account did not receive any transfer");
+        }
+
+        // Show every transfer information
+        StringBuilder responseBody = new StringBuilder();
+        for (Transfer transfer : receivedTransfers) {
+            responseBody.append("{\n\tid: ")
+                    .append(transfer.getTransferId())
+                    .append("\n\tdate: ")
+                    .append(transfer.getTransferDate())
+                    .append("\n\tcurrencyType: ")
+                    .append(transfer.getCurrencyType())
+                    .append("\n\tamount: ")
+                    .append(transfer.getTransferAmount())
+                    .append("\n\tstatus: ")
+                    .append(transfer.getTransferStatus())
+                    .append("\n}\n");
+        }
+        return ResponseEntity.ok(responseBody.toString());
     }
 }
--- src/main/java/org/example/api/service/TransferService.java
+++ src/main/java/org/example/api/service/TransferService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -67,5 +68,5 @@ public class TransferService {
         return ResponseEntity.ok().body("Transfer made successfully");
     }
 
-
+    public List<Transfer> findByReceivingAccount(Integer accountId) {return transferRepository.findByReceivingAccount_AccountId(accountId) ;}
 }
